3097201baab4d9a8374446f625fa78977eff0793
zxv <zxv@zxv>
2012-06-30 09:13:18 -0400

    Add some docs, override magic methods for logging.

--- Logger.php
+++ Logger.php
@@ -1,34 +1,149 @@
 <?php
+/*
+ * This project is comprised of two components:
+ * - A logger proxy class that acts as a proxy to any particular object.
+ * - A container logger class which holds bound logger proxies, allowing you to
+ *   inspect their state and call their methods.
+ *
+ * To begin, run this before the instantiation of the objects that you 
+ * wish to capture:
+ * $logger = Logger::getInstance();
+ * $logger->startLogger();
+ *
+ * If you want to stop logging temporarily, just execute Logger's 
+ * pauseLogger() method.
+ *
+ * IMPORTANT:
+ * The logger will not function properly unless Sebastian Bergmann's 
+ * php-test-helpers module is enabled in php.ini.
+ *
+ * Get it from https://github.com/sebastianbergmann/php-test-helpers/
+ * (For PHP 5.4 support, compile from the latest source instead of a release).
+ *
+ */
 
 class LoggerProxy {
+    private static $_initialized = false;
+    private static $_excludedNames = false;
+
     public function __construct() {
-        // Get the logger singleton
-        $logger = Logger::getInstance();
+        // Create a list of names which should not be proxied.
+        // This is checked using $this->_isExcluded() in the magic methods.
+        $self = get_class();
+        self::$_excludedNames = get_class_methods($self);
+        array_push(get_class_vars($self), self::$_excludedNames);
 
-        // Pause the logger to avoid an infinite loop
-        $logger->pauseLogger();
+        if (self::$_initialized != true) {
+            // Initialize an associative array to keep track of method calls
+            $this->_history = array();
 
-        // Get the most recent class name from those intercepted,
-        // instantiate a new instance of it.
+            // Get the logger singleton
+            $logger = Logger::getInstance();
+
+            // Pause the logger to avoid an infinite loop
+            $logger->pauseLogger();
+
+            // Get the most recent class name from those intercepted,
+            // instantiate a new instance of it.
+            
+            // I don't like how this is currently handled. I wish
+            // that I could pass the className to here straight from 
+            // the callback
+            $className = end($logger->_intercepted);
+            $this->obj = new $className;
+            if (!isset($this->obj)) {
+                throw new Exception("Error: Cannot create {$className} object");
+            }
+
+            // Append the current instance to live objects list in logger instance
+            $logger->liveObjects[] = $this;
+
+            // Finally, re-bind the logger's callback to the 'new' statement
+            $logger->startLogger();
+
+            self::$_initialized = true;
+            return true;
+        }
+
+        throw new Exception("Logger object already initialized");
+    }
+
+    public function setEvent($type, $data) {
+        // Log an event to the logger's internal array.
+        // $type is a string, and $data is an associative array.
+        
+        $entry = array($type => $data);
+        array_push($this->_history, $entry);
+    }
+
+    private function _isExcluded($methodOrProperty) {
+        // Determine whether a method or property is to be proxied.
+        //
+        // Conditions to avoid exclusion:
+        // 1. Is not a class variable or method
+        // 2. LoggerProxy's constructor has been executed
         // 
-        // I don't like how this is currently handled. I wish
-        // that I could pass the className to here straight from 
-        // the callback
-        $className = end($logger->_intercepted);
-        $this->obj = new $className;
+        if ( self::$_initialized == true ) {
+            if (!in_array($methodOrProperty, self::$_excludedNames)) {
+                return false;
+            }
+        }
 
-        // Append the current instance to live objects list
-        $logger->liveObjects[] = $this;
-
-        // Finally, re-bind the logger's callback to the 'new' statement
-        $logger->startLogger();
+        return true;
     }
 
-    public function __call($name, $arguments) {
-        echo "Called.";
-        die();
-        call_user_func_array($this->obj->$name, $arguments);
+    public function __call($method, $arguments) {
+        if (!$this->_isExcluded($method)) {
+            $this->setEvent("call", array("method" => $method, "args" => $arguments));
+
+            // Call the proxied object's method with the specified arguments
+            call_user_func_array(array($this->obj, $method), $arguments);
+        }
     }
+
+    public function __get($property) {
+        if (!$this->_isExcluded($property)) {
+            $entry = array("get" => array("property" => $property));
+            array_push($this->_history, $entry);
+
+            // Return the proxied object's property
+            return $this->obj->$property;
+        }
+
+        // Not sure whether this is required
+        //return $this->$property;
+    }
+
+    public function __set($property, $value) {
+        if (!$this->_isExcluded($property)) {
+            $this->setEvent("set", array("property" => $property, "value" => $value));
+            //array_push($this->_history[] = array("set" => array("property" => $property, "value" => $value)));
+
+            // Set the proxied object's property
+            $this->obj->$property = $value;
+
+            // We've created the object's property where it needs to stored.
+            // Prevent the setting of default property of the LoggerProxy object
+            return true;
+
+        }
+
+        $this->$property = $value;
+    }
+
+    public function __unset($property) {
+        if (!$this->_isExcluded($property)) {
+            $this->_history[] = array("unset" => array("property" => $property));
+
+            // Unset the proxied object's property
+            unset($this->obj->$property);
+            return;
+        }
+
+        // Not sure whether this is required
+        // unset($this->$property);
+    }
+
 }
 
 class Logger {
@@ -38,7 +153,7 @@ class Logger {
     private final function __clone() {}
 
     public static function getInstance() {
-        if ( !self::$uniqueInstance ) {
+        if (!self::$uniqueInstance) {
             self::$uniqueInstance = new Logger;
         }
 
@@ -56,11 +171,11 @@ class Logger {
     }
 
     public function startLogger($callback='intercept') {
-        if ( !$this->_intercepted ) {
+        if (!isset($this->_intercepted)) {
             $this->_intercepted = array();
         }
 
-        if ( !$this->liveObjects ) {
+        if (!isset($this->liveObjects)) {
             $this->liveObjects = array();
         }
 
